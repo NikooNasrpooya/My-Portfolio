c4f89808de918f97b59b43a6907259f1be5f161e
Nikoo Nasrpooya <nikoonasrpooya@yahoo.com>
2025-02-10 22:45:05 -0500

    site created

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover:' #fcf4ff',
+            darkHover:'#2a004a',
+            darkTheme: ' #11001F'
       },
+      fontFamily:{
+        Handlee : ["handlee"],
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
     },
   },
   plugins: [],
